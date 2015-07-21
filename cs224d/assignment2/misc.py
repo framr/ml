3cda647b35ec498496c33c42c4d0f056ba485046
Pavel Shtykovskiy <pavel.sht@yandex.ru>
2015-07-21 10:37:57 +0300

    minor changes

---
+++ cs224d/assignment2/misc.py
@@ -0,0 +1,12 @@
+##
+# Miscellaneous helper functions
+##
+
+import numpy as np
+
+def random_weight_matrix(m, n):
+    #### YOUR CODE HERE ####
+
+    #### END YOUR CODE ####
+    assert(A0.shape == (m,n))
+    return A0
